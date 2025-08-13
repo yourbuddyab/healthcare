237f3a2313a6d0f11c7181ce1c834ae6fb5f3765
Arjun Bhati <arjunbhati180@gmail.com>
2025-08-13 14:07:34 +0530

    feat(auth): add user login API

--- app/Http/Controllers/Api/v1/User/AuthController.php
+++ app/Http/Controllers/Api/v1/User/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\v1\User;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -25,5 +27,24 @@ class AuthController extends Controller
             'token'   => $token,
             'user'    => $user
         ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'status'  => false,
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $user['token'] = $user->createToken('API Token')->accessToken;
+
+        return response([
+            'status'  => true,
+            'message' => 'Login successful',
+            'data'   => $user
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('/auth/register', [AuthController::class, 'register']);
+    Route::post('/auth/login', [AuthController::class, 'login']);
 });
